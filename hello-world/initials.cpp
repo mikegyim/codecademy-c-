2bae9adbdc603fc6098ff5e791b1e2529b0bdbf4
Michael Opoku-Gyimah <60743701+mikegyim@users.noreply.github.com>
2020-05-24 09:28:38 -0400

    Add files via upload

---
+++ hello-world/initials.cpp
@@ -0,0 +1,13 @@
+//Fun Fact: I like going to the gym
+//My name initials
+#include<iostream>
+int main()
+{
+  std::cout << "M   M   OOO\n";
+  std::cout << "MM MM  O   O\n";
+  std::cout << "MM MM  O   O\n";
+  std::cout << "M M M  O   O\n";
+  std::cout << "M   M  O   O\n";
+  std::cout << "M   M  O   O\n";
+  std::cout << "M   M   OOO\n";
+}
